eb87267357a890f24d3e9ba660c24c30413f5b06
SzeChan <chansd5@gmail.com>
2015-06-11 18:45:50 -0400

    Security

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,15 +3,15 @@ class Task < ActiveRecord::Base
 
   # Sends a text message using secrets ENV and relays to twilio
   def send_text_message
-  	number_to_send_to = '+13473063012'
-    twilio_num = Rails.application.secrets.twilio_number
+  	number_to_send_to = 'somenum'
+    twilio_phone = Rails.application.secrets.twilio_phone
     twilio_account_sid = Rails.application.secrets.twilio_account_sid
     twilio_auth_token = Rails.application.secrets.twilio_auth_token
-    
+
     @client = Twilio::REST::Client.new twilio_account_sid, twilio_auth_token
 
   	@message = @client.messages.create( 
-      :from => twilio_num,
+      :from => twilio_phone,
   	  :to   => number_to_send_to,
   	  :body => 'What a piece of work is a man! How noble in reason, how infinite in faculty, in form and moving how express and admirable, in action how like an angel, in apprehension how like a god -- the beauty of the world, the paragon of animals!'
       )
