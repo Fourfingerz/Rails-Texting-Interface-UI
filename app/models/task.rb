b8d43f128c368971e812461e4add2d69b7fb94dd
SzeChan <chansd5@gmail.com>
2015-06-13 20:58:55 -0400

    texting works

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,10 +1,12 @@
 class Task < ActiveRecord::Base
   belongs_to :user
   belongs_to :recipient
+  after_create :schedule_sending_text
+  before_save :change_run_at
 
   # Sends a text message using secrets ENV and relays to twilio
   def send_text_message
-  	number_to_send_to = 'somenum'
+  	number_to_send_to = recipient.phone
     twilio_phone = ENV["TWILIO_PHONE_NUM"]
     twilio_account_sid = Rails.application.secrets.twilio_account_sid
     twilio_auth_token = Rails.application.secrets.twilio_auth_token
@@ -27,5 +29,11 @@ class Task < ActiveRecord::Base
   def schedule_sending_text
     job = self.delay(run_at: self.schedule_time).send_text_message
     update_column(:delayed_job_id, job.id)
+  end
+
+  def change_run_at
+    if schedule_time_changed? &&!new_record?
+      delayed_job.update_column(:run_at, schedule_time)
+    end
   end
 end
