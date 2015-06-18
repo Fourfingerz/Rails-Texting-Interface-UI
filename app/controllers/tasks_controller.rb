413f0e735b572456c3cedfe4e803874144ac02fd
SzeChan <chansd5@gmail.com>
2015-06-18 19:09:20 -0400

    Getting Tasks to properly receive Recipients that belong to User, first try

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -24,14 +24,14 @@ class TasksController < ApplicationController
   end
 
   def new
-    @task = Task.new(:user => User.first)
-    @recipients = @task.recipients
+    @task = Task.new
   end
 
   def create
-    @task = @current_user.tasks.build(task_params)
+    @task = Task.new(task_params)  
+    @task.user_id = current_user.id
     if @task.save
-      redirect_to tasks_url  
+      redirect_to user_path(session[:user_id])  
   	else
   	  render 'new'
   	end
