8468b14c9ad436f3ad241c7025b4f74b2bf6d8a4
SzeChan <chansd5@gmail.com>
2015-06-29 16:57:18 -0400

    Code cleanup in models

--- app/models/recipient.rb
+++ app/models/recipient.rb
@@ -3,17 +3,11 @@ class Recipient < ActiveRecord::Base
   has_many :ownerships, dependent: :destroy
   has_many :tasks, through: :ownerships
 
-  # has_many :ownerships, dependent: :destroy
-  # has_many :user_ownerships, through: :ownerships, class_name: "User", foreign_key: "user_id", source: :user
-  # accepts_nested_attributes_for :user_ownerships, :allow_destroy => true
-
-  # has_many :tasks, dependent: :destroy
-  # has_many :user_tasks, through: :tasks, class_name: "User", foreign_key: "user_id", source: :user
-
   validates :name, presence: true, uniqueness: true, length: { minimum: 5 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
 				    format: { with: VALID_EMAIL_REGEX },
 				    uniqueness: { case_sensitive: false }
   validates :phone, presence: true, length: { minimum: 7 }
+  validates :user_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,7 @@
 class User < ActiveRecord::Base
 	has_secure_password
-    validates_uniqueness_of :email
-    
 	has_many :tasks
 	has_many :recipients
-
-	# has_many :ownerships, dependent: :destroy
-	# has_many :recipient_ownerships, through: :ownerships, class_name: "Recipient", foreign_key: "recipient_id", source: :recipient
-
-	# has_many :tasks, dependent: :destroy
-	# has_many :recipient_tasks, through: :tasks, class_name: "Recipient", foreign_key: "recipient_id", source: :recipient
 
 	validates :name, presence: true, length: { minimum: 5 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
